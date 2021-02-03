397284eb4488029189689f5448678680d408d5c4
yves <kabiryves90@gmail.com>
2021-02-03 18:35:39 +0200

    update show roles

--- resources/views/roles/show.blade.php
+++ resources/views/roles/show.blade.php
@@ -1,7 +1,6 @@
-@extends('layouts.app')
-
-
+@extends('layouts.layout')
 @section('content')
+@section('title','Roles | '.config('app.name'))
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
