f54925cdd0d37416ad3b9bd2e7cacded30d99865
yves <kabiryves90@gmail.com>
2021-04-03 20:58:28 +0200

    updat script

--- resources/views/base/script.blade.php
+++ resources/views/base/script.blade.php
@@ -1,14 +1,14 @@
 <!-- Bootstrap core JavaScript-->
 <script src="{{ asset('assets/js/jquery.min.js')}}"></script>
+<script src="{{ asset('plugins/bootstrap/js/bootstrap.min.css')}}"></script>
 <script src="{{ asset('assets/js/popper.min.js')}}"></script>
-<script src="{{ asset('assets/js/bootstrap.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+ <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+ <!-- AdminLTE App -->
+ <script src="{{ asset('dist/js/adminlte.min.js')}}"></script>
 
-<!-- simplebar js -->
-<script src="{{ asset('assets/plugins/simplebar/js/simplebar.js')}}"></script>
 <!-- waves effect js -->
 <script src="{{ asset('assets/js/waves.js')}}"></script>
-<!-- sidebar-menu js -->
-<script src="{{ asset('assets/js/sidebar-menu.js')}}"></script>
 <!-- Custom scripts -->
 <script src="{{ asset('assets/js/app-script.js')}}"></script>
 
@@ -49,17 +49,3 @@
       } );
 
     </script>
-
-
-<!-- Vector map JavaScript -->
-<script src="{{ asset('assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js')}}"></script>
-<script src="{{ asset('assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
-<!-- Sparkline JS -->
-<script src="{{ asset('assets/plugins/sparkline-charts/jquery.sparkline.min.js')}}"></script>
-<!-- Chart js -->
-<script src="{{ asset('assets/plugins/Chart.js/Chart.min.js')}}"></script>
-<!--notification js -->
-<script src="{{ asset('assets/plugins/notifications/js/lobibox.min.js')}}"></script>
-<script src="{{ asset('assets/plugins/notifications/js/notifications.min.js')}}"></script>
-<!-- Index js -->
-<script src="{{ asset('assets/js/index2.js')}}"></script>
