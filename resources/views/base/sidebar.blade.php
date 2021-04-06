f65b23497d3cfbab638e6a3cdc704c3c80331e69
yves <kabiryves90@gmail.com>
2021-04-06 11:50:48 +0200

    add link for monnaie

--- resources/views/base/sidebar.blade.php
+++ resources/views/base/sidebar.blade.php
@@ -31,7 +31,7 @@
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a href="#" class="nav-link">
+                  <a href="{{ url('monnaies') }}" class="nav-link">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Monnaie</p>
                   </a>
