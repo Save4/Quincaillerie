6fa59ead4eab9526485116677014b726d82941da
yves <kabiryves90@gmail.com>
2021-04-06 14:18:11 +0200

    add view magasin

---
+++ resources/views/magasins/index.blade.php
@@ -0,0 +1,167 @@
+@extends('layouts.layout')
+@section('content')
+@section('title', 'Magasin | ' . config('app.name'))
+
+    <div class="container-fluid">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-head">
+                            <h4 style="float: left">Ajouter un magasin</h4>
+                            <a href="" style="float: right" class="btn btn-primary" data-toggle="modal"
+                                data-target="#addmagasin">
+                                <i class="fa fa-plus"></i>Ajout d'un magasin</a>
+                        </div>
+                        <div class="card-body">
+                            <table id="default-datatable" class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Magasin</th>
+                                        <th>Localite</th>
+                                        <th>Email</th>
+                                        <th>Telephone</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($magasins as $key => $magasin)
+                                        <tr>
+                                            <td>{{ $key + 1 }}</td>
+                                            <td>{{ $magasin->nom_magasin }}</td>
+                                            <td>{{ $magasin->localite }}</td>
+                                            <td>{{ $magasin->email }}</td>
+                                            <td>{{ $magasin->phone }}</td>
+                                            <td>
+                                                <div class="btn-group">
+                                                    <a href="" class="btn btn-sm btn-info" data-toggle="modal"
+                                                        data-target="#editmagasin{{ $magasin->id }}"><i
+                                                            class="fa fa-edit"></i>Edit</a>
+                                                    <a href="" class="btn btn-sm btn-danger" data-toggle="modal"
+                                                        data-target="#deletemagasin{{ $magasin->id }}"><i
+                                                            class="fa fa-trash"></i>Delete</a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- modal of adding product -->
+    <div class="modal right fade" id="addmagasin" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header bg-primary">
+                    <h4 class="modal-title" id="staticBackdropLabel">Ajout de magasin</h4>
+                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('magasins.store') }}" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <label for="">Magasin</label>
+                            <input type="text" name="nom_magasin" id="" class="form-control" placeholder="magasin"
+                                aria-describedby="helpId">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Localite</label>
+                            <input type="text" name="localite" id="" class="form-control" placeholder="localite"
+                                aria-describedby="helpId">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Email</label>
+                            <input type="email" name="email" id="" class="form-control" placeholder="email"
+                                aria-describedby="helpId">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Telephone</label>
+                            <input type="text" name="phone" id="" class="form-control" placeholder="phone"
+                                aria-describedby="helpId">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="" name="" id="" class="btn btn-primary btn-block">Enregistre</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- modal of edit product-->
+
+    <div class="modal right fade" id="editmagasin{{ $magasin->id }}" data-bs-backdrop="static" data-bs-keyboard="false"
+        tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header bg-primary">
+                    <h4 class="modal-title" id="staticBackdropLabel">Modifier le magasin</h4>
+                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('magasins.update', $magasin->id) }}" method="POST">
+                        @csrf
+                        @method('put')
+                        <div class="form-group">
+                            <label for="">Magasin</label>
+                            <input type="text" name="nom_magasin" id="" value="{{ $magasin->nom_magasin }}"
+                                class="form-control" placeholder="" aria-describedby="helpId">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Localite</label>
+                            <input type="text" name="localite" id="" value="{{ $magasin->localite }}"
+                                class="form-control" placeholder="" aria-describedby="helpId">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Email</label>
+                            <input type="email" name="email" id="" value="{{ $magasin->email }}"
+                                class="form-control" placeholder="" aria-describedby="helpId">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Telephone</label>
+                            <input type="text" name="phone" id="" value="{{ $magasin->phone }}"
+                                class="form-control" placeholder="" aria-describedby="helpId">
+                        </div>
+                        <div class="modal-footer">
+                            <button class="btn btn-warning btn-block">Modifier</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- modal of delete product-->
+
+    <div class="modal right fade" id="deletemagasin{{ $magasin->id }}" data-bs-backdrop="static" data-bs-keyboard="false"
+        tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header bg-primary">
+                    <h4 class="modal-title" id="staticBackdropLabel">Supprimer le magasin</h4>
+                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('magasins.destroy', $magasin->id) }}" method="POST">
+                        @csrf
+                        @method('delete')
+                        <p>Tu es sur de vouloir supprimer magasin {{ $magasin->nom_magasin }} ?</p>
+                        <div class="modal-footer">
+                            <button type="" name="" id="" class="btn btn-info" data-dismiss="modal">Cancel</button>
+                            <button type="submit" name="" id="" class="btn btn-danger">Delete</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endsection
