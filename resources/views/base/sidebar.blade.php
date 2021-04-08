36d2e51ff8b070352525e7fe26180e3f10e4e19a
yves <kabiryves90@gmail.com>
2021-04-08 08:37:00 +0200

    add link for fourniseur

--- resources/views/base/sidebar.blade.php
+++ resources/views/base/sidebar.blade.php
@@ -49,7 +49,7 @@
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a href="#" class="nav-link">
+                  <a href="{{url('fournisseurs')}}" class="nav-link">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Fournisseur</p>
                   </a>
