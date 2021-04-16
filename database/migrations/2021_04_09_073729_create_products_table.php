f2266372a35ea21d78dba6eb5ecdac1653da7417
yves <kabiryves90@gmail.com>
2021-04-16 13:03:27 +0200

    update table products

---
+++ database/migrations/2021_04_09_073729_create_products_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('monnaie_id');
+            $table->unsignedBigInteger('unite_id');
+            $table->unsignedBigInteger('magasin_id');
+            $table->unsignedBigInteger('fournisseur_id');
+            $table->string('product_name');
+            $table->text('description');
+            $table->string('brand');
+            $table->integer('price');
+            $table->integer('quantity');
+            $table->integer('alert_stock')->default(100);
+            $table->timestamps();
+            $table->foreign('monnaie_id')
+                ->references('id')
+                ->on('monnaies')
+                ->onDelete('cascade');
+            $table->foreign('unite_id')
+                ->references('id')
+                ->on('unites')
+                ->onDelete('cascade');
+            $table->foreign('magasin_id')
+                ->references('id')
+                ->on('magasins')
+                ->onDelete('cascade');
+            $table->foreign('fournisseur_id')
+                ->references('id')
+                ->on('fournisseurs')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
