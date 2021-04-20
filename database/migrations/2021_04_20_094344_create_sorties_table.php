8c350ea26a189ad838325d4677fa11ce214bb649
yves <kabiryves90@gmail.com>
2021-04-20 13:03:45 +0200

    create table sortie

---
+++ database/migrations/2021_04_20_094344_create_sorties_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSortiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sorties', function (Blueprint $table) {
+            $table->id();
+            $table->integer('client_id');
+            $table->integer('product_id');
+            $table->integer('quantity');
+            $table->integer('unitprice');
+            $table->integer('amount');
+            $table->integer('discount');
+            $table->timestamps();
+            $table->foreign('client_id')
+                ->references('id')
+                ->on('clients')
+                ->onDelete('cascade');
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sorties');
+    }
+}
