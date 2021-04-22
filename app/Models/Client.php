57eec128049b409d54ce027e8492d135a5188e2e
yves <kabiryves90@gmail.com>
2021-04-22 20:29:54 +0200

    update client model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
-    protected $table = 'orders';
+    protected $table = 'clients';
     protected $fillable = ['name', 'address'];
 }
