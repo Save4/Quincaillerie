1b10a12b8c4f3855a9cae7439186e1d2be4428a6
yves <kabiryves90@gmail.com>
2021-05-06 11:11:10 +0200

    create vue repport

---
+++ resources/views/reports/receipt.blade.php
@@ -0,0 +1,86 @@
+{{-- prnt section --}}
+
+<div id="invoice">
+    <div id="printed_content">
+        <center id="top">
+            <div class="logo"></div>
+            <div class="info"></div>
+            <h2>Quincaillerie</h2>
+        </center>
+    </div>
+
+    <div class="mid">
+        <div class="info">
+            <h2>Contact</h2>
+            <p>
+                Nom: NINDORERE Jeanne
+                Addresse: Buyenzi
+                Email: nidorere,jeanne@gmail.com
+                Telephone: 79000000,69000000
+            </p>
+        </div>
+    </div>
+
+    {{-- End of receipt mid --}}
+
+    <div class="bot">
+        <div class="table">
+            <table>
+                <tr class="tabletitle">
+                    <td class="item"><h2>Iteam</h2></td>
+                    <td class="Hours"><h2>QTY</h2></td>
+                    <td class="Rate"><h2>Unit</h2></td>
+                    <td class="Rate"><h2>Discount</h2></td>
+                    <td class="Rate"><h2>Sub Total</h2></td>
+                </tr>
+                <tr class=service>
+                    <td class="tableitem">
+                        <p class="itemtex">Mongo</p>
+                    </td>
+                    <td class="tableitem">
+                        <p class="itemtex">20</p>
+                    </td>
+                    <td class="tableitem">
+                        <p class="itemtex">$5</p>
+                    </td>
+                    <td class="tableitem">
+                        <p class="itemtex">0</p>
+                    </td>
+                    <td class="tableitem">
+                        <p class="itemtex">$100</p>
+                    </td>
+                </tr>
+                <tr class="tabletitle">
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td class="Rate">
+                        <p class="itemtex">Tax</p>
+                    </td>
+                    <td class="Payement">
+                        <p class="itemtex">$100</p>
+                    </td>
+                </tr>
+                <tr class="tabletitle">
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td class="Rate">Total</td>
+                    <td class="Payement">
+                        <h2>$100</h2>
+                    </td>
+                </tr>
+            </table>
+            <div class="legalcopy">
+                <p class="legal"><strong>** Merci **</strong><br> hello</p>
+            </div>
+            <div class="serial-number">
+                Serial: <span class="serial">78678896</span>
+                <span>24/12/2020 &nbsp; &nbsp; 00:04</span>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
